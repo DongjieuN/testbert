eb8236200fe4d99e8d3a8a95dcd6a9299c0f2a08
DongjieuN <122781952+DongjieuN@users.noreply.github.com>
2023-10-02 15:13:34 +0900

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,16 +1,14 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
-
 import numpy as np; np.random.seed(1234)
 import pandas as pd
 
-
 ntrain = 150000
 
-data = pd.read_csv('../ratings.txt', sep='\t', quoting=3)
+# 파일 경로는 실제 파일 위치에 따라 변경해야 합니다.
+data = pd.read_csv('../ratings.txt', sep='\t', header=None, names=['document', 'label'])
 data = pd.DataFrame(np.random.permutation(data))
 trn, tst = data[:ntrain], data[ntrain:]
 
-header = 'id document label'.split()
+header = ['document', 'label']
 trn.to_csv('../ratings_train.txt', sep='\t', index=False, header=header)
 tst.to_csv('../ratings_test.txt', sep='\t', index=False, header=header)
+

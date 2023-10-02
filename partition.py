b6d8e18e9566aebb6fbdeb3f25e2276aa5ee35cf
DongjieuN <122781952+DongjieuN@users.noreply.github.com>
2023-10-02 15:29:35 +0900

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
 # 파일 경로는 실제 파일 위치에 따라 변경하세요.
-train = pd.read_csv("testbert/ratings_train.txt", sep='\t', header=None)
-test = pd.read_csv("testbert/ratings_test.txt", sep='\t', header=None)
-
-train.columns = ['document', 'label']
-test.columns = ['document', 'label']
+train = pd.read_csv("testbert/ratings_train.txt", sep='\t', header=None, names=['document', 'label'])
+test = pd.read_csv("testbert/ratings_test.txt", sep='\t', header=None, names=['document', 'label'])
 
 print(train)
 print(test)

a299aae3bbba8700fa42c891685c6bb734fe7c09
DongjieuN <122781952+DongjieuN@users.noreply.github.com>
2023-10-02 15:29:17 +0900

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-# 가정: 각 줄은 탭으로 구분된 document와 label 정보를 가지고 있음
-data = pd.read_csv('파일경로.txt', sep='\t', header=None, names=['document'])
+# 파일 경로는 실제 파일 위치에 따라 변경하세요.
+train = pd.read_csv("testbert/ratings_train.txt", sep='\t', header=None)
+test = pd.read_csv("testbert/ratings_test.txt", sep='\t', header=None)
 
-# document 열에서 마지막 문자(라벨 정보) 분리
-data['label'] = data['document'].str[-1].astype(int)
-data['document'] = data['document'].str[:-2]
+train.columns = ['document', 'label']
+test.columns = ['document', 'label']
 
-print(data)
-
+print(train)
+print(test)

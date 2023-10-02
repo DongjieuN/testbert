88b89fa87c8ffb6b145e55fb36a3c61a9232a057
DongjieuN <122781952+DongjieuN@users.noreply.github.com>
2023-10-02 15:25:15 +0900

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,14 +1,11 @@
-import numpy as np; np.random.seed(1234)
 import pandas as pd
 
-ntrain = 150000
+# 가정: 각 줄은 탭으로 구분된 document와 label 정보를 가지고 있음
+data = pd.read_csv('파일경로.txt', sep='\t', header=None, names=['document'])
 
-# 파일 경로는 실제 파일 위치에 따라 변경해야 합니다.
-data = pd.read_csv('../ratings.txt', sep='\t', header=None, names=['document', 'label'])
-data = pd.DataFrame(np.random.permutation(data))
-trn, tst = data[:ntrain], data[ntrain:]
+# document 열에서 마지막 문자(라벨 정보) 분리
+data['label'] = data['document'].str[-1].astype(int)
+data['document'] = data['document'].str[:-2]
 
-header = ['document', 'label']
-trn.to_csv('../ratings_train.txt', sep='\t', index=False, header=header)
-tst.to_csv('../ratings_test.txt', sep='\t', index=False, header=header)
+print(data)
 
